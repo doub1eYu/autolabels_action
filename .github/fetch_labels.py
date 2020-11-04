373bd59858ad58c9f63c7ee72ba9f55f57df43d5
s-so <s-so@media-plex.net>
2020-11-04 13:39:43 +0900

    ssss

--- .github/fetch_labels.py
+++ .github/fetch_labels.py
@@ -6,9 +6,9 @@ import toml
 dict_toml = toml.load(open('.github/labels.toml'))
 dict_toml_f = toml.load(open('.github/labels_first_time.toml'))
 
-for k in dict_toml:
-    if k in dict_toml_f:
-        dict_toml[k] = dict_toml_f[k]
+for k in dict_toml_f:
+    dict_toml[k] = dict_toml_f[k]
+    
 print(dict_toml)
 toml.dump(dict_toml, open('.github/labels.toml', mode='w'))
 
