0a33f2e7e2696bdcc22a946abda383c5ec1bdb21
s-so <s-so@media-plex.net>
2020-11-04 13:55:57 +0900

    ssss

--- .github/fetch_labels.py
+++ .github/fetch_labels.py
@@ -2,16 +2,25 @@
 # coding: utf-8
 
 import toml
+import sys
+import os
+import subprocess
 
+try:
+    dict_toml_f = toml.load(open('.github/labels_first_time.toml'))
+except Exception:
+    sys.exit()
 dict_toml = toml.load(open('.github/labels.toml'))
-dict_toml_f = toml.load(open('.github/labels_first_time.toml'))
+
+cmd = "labels fetch -f .github/labels.toml"
+runcmd = subprocess.check_output(cmd.split())
+print(runcmd)
 
 for k in dict_toml_f:
     dict_toml[k] = dict_toml_f[k]
-    
 print(dict_toml)
 toml.dump(dict_toml, open('.github/labels.toml', mode='w'))
-
+os.remove('.github/labels_first_time.toml')
 
 
 
