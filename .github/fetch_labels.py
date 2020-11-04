748a74ff821d82a880a68616f3b9c91312a7d819
s-so <s-so@media-plex.net>
2020-11-04 14:00:54 +0900

    ssss

--- .github/fetch_labels.py
+++ .github/fetch_labels.py
@@ -3,24 +3,26 @@
 
 import toml
 import sys
-import os
 import subprocess
 
 try:
     dict_toml_f = toml.load(open('.github/labels_first_time.toml'))
 except Exception:
     sys.exit()
-dict_toml = toml.load(open('.github/labels.toml'))
 
 cmd = "labels fetch -f .github/labels.toml"
 runcmd = subprocess.check_output(cmd.split())
 print(runcmd)
+dict_toml = toml.load(open('.github/labels.toml'))
 
 for k in dict_toml_f:
     dict_toml[k] = dict_toml_f[k]
 print(dict_toml)
 toml.dump(dict_toml, open('.github/labels.toml', mode='w'))
-os.remove('.github/labels_first_time.toml')
+
+cmd = "git rm .github/labels_first_time.toml"
+runcmd = subprocess.check_output(cmd.split())
+print(runcmd)
 
 
 
